995e90f49e9c46f2f10f8defffa4b633b2568dd3
NicoSnAong <nicolas.snejkovsky@gmail.com>
2025-09-27 17:33:06 +0700

    test

---
+++ script.py
@@ -0,0 +1,15 @@
+# On importe un module standard de Python : pathlib
+# pathlib sert à manipuler les fichiers et dossiers de manière simple et portable.
+from pathlib import Path
+
+
+# Path.cwd() renvoie le dossier actuel (là où le script est lancé)
+dossier = Path.cwd()
+
+
+# On définit un chemin vers un fichier hello.txt dans ce dossier
+fichier =dossier/"demo.txt"
+
+
+# On écrit du texte dans ce fichier
+fichier.write_text("hello les amis\n", encoding="utf-8")
